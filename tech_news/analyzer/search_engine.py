023bb3136a9a5ba06a4cf8241a59965e69eb8076
davi <davijluna@gmail.com>
2023-01-23 14:55:49 -0300

    feat: requidito 6 implementando função search_by_title

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,6 +1,13 @@
+from tech_news.database import search_news
+
 # Requisito 6
+
+
 def search_by_title(title):
     """Seu código deve vir aqui"""
+    response = {"title": {"$regex": title, "$options": "i"}}
+    response_new = search_news(response)
+    return [(new["title"], new["url"]) for new in response_new]
 
 
 # Requisito 7
