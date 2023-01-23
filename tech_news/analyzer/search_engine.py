388f8a4c0f5116e05d7a76715e823efb05e4e2a1
davi <davijluna@gmail.com>
2023-01-23 16:47:13 -0300

    feat: requisito 9 implementando função search_by_category

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -36,4 +36,10 @@ def search_by_tag(tag):
 
 # Requisito 9
 def search_by_category(category):
+
+    new = []
+    response = search_news({"category": {"regex": category, "$options": "i"}})
+    for index in response:
+        new.append((index["title"], index["url"]))
+    return new
     """Seu código deve vir aqui"""
